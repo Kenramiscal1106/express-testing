58031b368cdcd50d263d0bb4f0bf44ae7322733c
Kenramiscal1106 <ramiscalken@gmail.com>
2022-06-22 10:39:02 +0800

    - removed log - changed logic of /readme/:component endpoint - added /test/:component

--- src/index.js
+++ src/index.js
@@ -15,15 +15,49 @@ app.get("/readme", (req,res) => {
       res.send(err)
       return 
     }
-    console.log(data)
     res.send(marked.parse(data))
   })
 })
 app.get("/readme/:component", (req, res) => {
-  console.log(req.params)
-  res.send(req.params)
+  // req.params.component
+  const imageDir = path.join(cwd(), "src", "img");
+  fs.readdir(imageDir, (err, files) => {
+    if (err) {
+      res.send("non-existent directory")
+      return
+    }
+    const reqFileDir = files
+      .filter(file => /\.png/.test(file))
+      .map(file => file.replace(/(\.png| )/g, ""))
+    const reqFile = reqFileDir.find(img => req.params.component === img.toLowerCase());
+    if (!reqFile) {
+      res.status(404).send("image doesn't exist")
+      return
+    }
+    const file = fs.createReadStream(imageDir + "/" + reqFile + ".png")
+    file.on("data", (chunk) => {
+      res.setHeader("Content-type", "image/png")
+      res.send(chunk)
+    })
+  })
 },)
-
+app.get("/test/:component", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>${req.params.component}</h1>
+      <img src="http://localhost:5000/readme/${req.params.component}" alt="an image that shoud appear"/>
+    </body>
+    </html>
+  `)
+})
 app.get("/", (req, res) => {
   res.redirect("/readme")
 })
